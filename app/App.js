7b546ae533e350a33b8f2733ab2864778660acad
Gustavo Barbosa <gusbdev@gmail.com>
2020-11-01 14:48:52 -0300

    Component changed to Functional Component

--- app/App.js
+++ app/App.js
@@ -1,32 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 import Routes from './src/navigation';
-import { Block } from './src/components';
-import { AuthProvider } from './src/contexts/auth';
+import {Block} from './src/components';
+import {AuthProvider} from './src/contexts/auth';
 import CartProvider from './src/contexts/cart';
 
-export default class App extends React.Component {
-
-  render(){
-    return (
-      <Block white>
-        <AuthProvider>
-          <CartProvider>
-            <Routes />
-          </CartProvider>
-        </AuthProvider>
-      </Block>
-    );
-  }
-  
+export default function App() {
+  return (
+    <Block white>
+      <AuthProvider>
+        <CartProvider>
+          <Routes />
+        </CartProvider>
+      </AuthProvider>
+    </Block>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
